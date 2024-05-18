d205b4a63e6d0c6e7ae41c0049f748de56e6f428
Twentyfive-lloyd <68084439+Twentyfive-lloyd@users.noreply.github.com>
2024-05-18 20:05:57 +0100

    Add files via upload

--- database_connection.py
+++ database_connection.py
@@ -12,8 +12,8 @@ def connect_to_database():
             conn = mysql.connector.connect(
                 host="127.0.0.1",
                 database="ngdelivery",
-                user="root",
-                password=""
+                user="kmfktg",
+                password="Defs@2012.com"
             )
             return conn  # Retourne la connexion si la connexion réussit
         except mysql.connector.Error as error:
@@ -24,33 +24,3 @@ def connect_to_database():
     
     logging.error("Impossible de se connecter à la base de données après plusieurs tentatives.")
     return None  # Retourne None si la connexion échoue après les tentatives
-
-# Classe Singleton pour gérer la connexion à la base de données
-class DatabaseConnection:
-    _instance = None
-
-    def __new__(cls):
-        if cls._instance is None:
-            cls._instance = super().__new__(cls)
-            cls._instance._conn = cls._instance.connect_to_database()
-        return cls._instance
-
-    def connect_to_database(self):
-        try:
-            conn = mysql.connector.connect(
-                host="127.0.0.1",
-                database="ngdelivery",
-                user="root",
-                password=""
-            )
-            logging.info("Connexion à la base de données établie avec succès")
-            return conn
-        except mysql.connector.Error as e:
-            logging.error("Erreur lors de la connexion à la base de données: %s", e)
-            return None
-
-    @property
-    def conn(self):
-        if self._conn is None or not self._conn.is_connected():
-            self._conn = self.connect_to_database()
-        return self._conn
